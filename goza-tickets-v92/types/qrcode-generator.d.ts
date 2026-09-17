b553cf62070c3d0e107bb00519dd20e598632102
Ghostinsky1 <jhuaroco@gmail.com>
2026-09-17 02:14:53 -0500

    Add files via upload

---
+++ goza-tickets-v92/types/qrcode-generator.d.ts
@@ -0,0 +1,7 @@
+declare module 'qrcode-generator' {
+  function qrcode(typeNumber: number, errorCorrectionLevel: string): {
+    addData(data: string): void; make(): void;
+    createDataURL(cellSize?: number, margin?: number): string;
+  };
+  export default qrcode;
+}
